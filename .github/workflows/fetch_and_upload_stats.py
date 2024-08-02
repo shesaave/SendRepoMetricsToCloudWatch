02f3d0664aba4c24c94ef92aa4fe262f5256744c
Sherlyn <shesaave@amazon.com>
2024-08-02 17:23:02 -0400

    Update fetch_and_upload_stats.py

--- .github/workflows/fetch_and_upload_stats.py
+++ .github/workflows/fetch_and_upload_stats.py
@@ -64,7 +64,7 @@ def fetch_num_closed_issues():
   return fetch_data(url_closed_issues)
 
 def fetch_num_closed_prs_yesterday():
-  today = datetime.datetime.now()
+  today = datetime.now()
   yesterday = today - timedelta(days=1)
   start_of_yesterday = datetime.combine(yesterday, datetime.min.time())
   end_of_yesterday = datetime.combine(yesterday, datetime.max.time())
