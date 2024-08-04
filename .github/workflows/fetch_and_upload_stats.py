a3949ba782de6b765130f718c2ee46202c565a5a
Sherlyn Saavedra <shesaave@amazon.com>
2024-08-04 13:55:58 -0400

    testing

--- .github/workflows/fetch_and_upload_stats.py
+++ .github/workflows/fetch_and_upload_stats.py
@@ -65,11 +65,10 @@ def fetch_num_closed_issues():
 
 def fetch_num_closed_prs_yesterday():
   today = datetime.utcnow().date()
-  start_of_year = datetime(today.year, 1, 1)
-  end_of_year = datetime(today.year, 12, 31)
-  url_closed_prs_this_year = f"{GITHUB_API_URL}/repos/aws-actions/{REPO_NAME}/pulls?state=closed&since={start_of_year.isoformat()}&until={end_of_year.isoformat()}"
-  return fetch_data(url_closed_prs_this_year)
-
+  start_of_week = today - timedelta(days=today.weekday())
+  end_of_week = start_of_week + timedelta(days=6)
+  url_closed_prs_this_week = f"{GITHUB_API_URL}/repos/aws-actions/{REPO_NAME}/pulls?state=closed&since={start_of_week.isoformat()}&until={end_of_week.isoformat()}"
+  return fetch_data(url_closed_prs_this_week)
 def upload_metrics_to_cloudwatch(num_issues, num_prs_open, num_prs_closed_yesterday):
 
   if num_issues is not None and num_prs_open is not None:
