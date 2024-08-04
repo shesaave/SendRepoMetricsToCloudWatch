71ceb32a457854cc2508d610ca6e1d605174cddb
Sherlyn Saavedra <shesaave@amazon.com>
2024-08-04 01:02:56 -0400

    testing

--- .github/workflows/fetch_and_upload_stats.py
+++ .github/workflows/fetch_and_upload_stats.py
@@ -67,8 +67,8 @@ def fetch_num_closed_prs_yesterday():
   today = datetime.utcnow().date()
   start_of_week = today - timedelta(days=today.weekday())
   end_of_week = start_of_week + timedelta(days=7)
-  start_of_week_datetime = datetime.combine(start_of_week, tzinfo=timezone.utc)
-  end_of_week_datetime = datetime.combine(end_of_week, tzinfo=timezone.utc)
+  start_of_week_datetime = datetime.combine(start_of_week, tzinfo=datetime.timezone.utc)
+  end_of_week_datetime = datetime.combine(end_of_week, tzinfo=today)
   url_closed_prs_this_week = f"{GITHUB_API_URL}/repos/aws-actions/{REPO_NAME}/pulls?state=closed&since={start_of_week_datetime.isoformat()}&until={end_of_week_datetime.isoformat()}"
   return fetch_data(url_closed_prs_this_week)
 
