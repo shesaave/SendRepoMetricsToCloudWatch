d698c6ab241be80c7994b1445273cd0fa024dc55
Sherlyn Saavedra <shesaave@amazon.com>
2024-08-04 12:58:17 -0400

    testing

--- .github/workflows/fetch_and_upload_stats.py
+++ .github/workflows/fetch_and_upload_stats.py
@@ -64,13 +64,16 @@ def fetch_num_closed_issues():
   return fetch_data(url_closed_issues)
 
 def fetch_num_closed_prs_yesterday():
+  current_year = datetime.utcnow().year
   today = datetime.utcnow().date()
-  current_year = today.year
-  current_week = today.isocalendar()[1]
-  start_of_week_datetime = datetime.strptime(f"{current_year}-W{current_week}-1", "%Y-W%W-%w")
-  end_of_week_datetime = start_of_week_datetime + timedelta(days=6) 
-  url_closed_prs_this_week = f"{GITHUB_API_URL}/repos/aws-actions/{REPO_NAME}/pulls?state=closed&since={start_of_week_datetime.isoformat()}&until={end_of_week_datetime.isoformat()}"
-  return fetch_data(url_closed_prs_this_week)
+    if today.year!= current_year:
+        print("This code is only intended to run in the current year.")
+        return None
+  yesterday = today - timedelta(days=1)
+  start_of_yesterday = datetime.combine(yesterday, datetime.min.time())
+  end_of_yesterday = datetime.combine(yesterday, datetime.max.time())
+  url_closed_prs_yesterday = f"{GITHUB_API_URL}/repos/aws-actions/{REPO_NAME}/pulls?state=closed&since={start_of_yesterday.isoformat()}&until={end_of_yesterday.isoformat()}"
+  return fetch_data(url_closed_prs_yesterday)
 
 def upload_metrics_to_cloudwatch(num_issues, num_prs_open, num_prs_closed_yesterday):
 
